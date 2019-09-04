864b8bd73efb4b4aad411492d4c83606f6840bd3
unknown <rmpiper96@gmail.com>
2019-09-04 09:32:57 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.kanye.rest";
+
+            HttpClient client = new HttpClient();
+
+            string response = client.GetStringAsync(url).Result;
+
+            string joke2 = JObject.Parse(response).GetValue("quote").ToString();
+
+            Console.WriteLine(joke2);
+        }
+    }
+}
